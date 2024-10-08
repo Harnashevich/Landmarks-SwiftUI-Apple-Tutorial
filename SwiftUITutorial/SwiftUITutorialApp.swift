f688500b2a9ef6c3f852090da2138333afb6d875
Andrei <garnash1313@gmail.com>
2024-10-08 13:29:47 +0300

    Initial Commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Andrei Harnashevich on 8.10.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUITutorialTests/SwiftUITutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialTests.swift
+//  SwiftUITutorialTests
+//
+//  Created by Andrei Harnashevich on 8.10.24.
+//
+
+import Testing
+@testable import SwiftUITutorial
+
+struct SwiftUITutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
