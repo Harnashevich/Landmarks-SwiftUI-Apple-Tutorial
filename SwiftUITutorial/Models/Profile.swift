d833716928ffc54cb31a4200c48da06861f1f198
Andrei <garnash1313@gmail.com>
2024-11-14 14:51:42 +0300

    Profile

---
+++ SwiftUITutorial/Models/Profile.swift
@@ -0,0 +1,30 @@
+//
+//  Profile.swift
+//  SwiftUITutorial
+//
+//  Created by Andrei Harnashevich on 14.11.24.
+//
+
+import Foundation
+
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+
+    static let `default` = Profile(username: "g_kumar")
+
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+
+        var id: String { rawValue }
+    }
+}
